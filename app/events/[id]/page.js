304d34487d6f39cc0c253cb78d30c6e5c37a755a
higirobruce <higirobru@gmail.com>
2023-07-06 22:08:21 +0200

    loading states

--- app/events/[id]/page.js
+++ app/events/[id]/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import useNextBlurhash from "use-next-blurhash";
 
 import { useParams } from "next/navigation";
 import Image from "next/image";
@@ -64,168 +65,209 @@ export const events = [
   },
 ];
 
-export async function getEventDetails(ticketId) {
+export async function getEventDetails(ticketId, setLoadingDetails) {
+  setLoadingDetails(true);
   return fetch(`${url}/events/${ticketId}`, {
     headers: {
       Content: "application/json",
     },
-  })
+  });
 }
 export default function Event({ params }) {
   let [event, setEvent] = useState(null);
+  const [blurDataUrl] = useNextBlurhash("L6PZfSi_.AyE_3t7t7R**0o#DgR4");
+  let [loadingEventDetails, setLoadingEventDetails] = useState(true);
 
   useEffect(() => {
-    getEventDetails(params?.id).then(res=>{
-      if(res.ok){
-        return res.json()
-      } else{
-        console.log(res.statusText)
-      }
-    }).then(res=>{
-      setEvent(res)
-    })
+    getEventDetails(params?.id, setLoadingEventDetails)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log(res.statusText);
+        }
+      })
+      .then((res) => {
+        setEvent(res);
+      })
+      .finally(() => {
+        setLoadingEventDetails(false);
+      });
   }, [params]);
 
   const breakLine = <div className="h-[0.5px] w-full bg-gray-200"></div>;
   return (
-    <div className="grid grid-cols-3 py-5 px-36 gap-10">
-      <div className="col-span-2 flex flex-col">
-        <div className="w-full">
-          <Image
-            src={event?.imageUrl}
-            className="rounded-md"
-            height={500}
-            width={1000}
-          />
-        </div>
+    <>
+      {!loadingEventDetails && (
+        <div className="grid grid-cols-3 py-5 px-36 gap-10">
+          <div className="col-span-2 flex flex-col">
+            <div className="w-full">
+              <Image
+                src={event?.imageUrl}
+                className="rounded-md"
+                height={500}
+                width={1000}
+                placeholder="blur"
+                blurDataURL={blurDataUrl}
+                alt="Picture of the author"
+              />
+            </div>
 
-        <div className="rounded-md bg-white flex flex-col py-5 px-8 mt-5">
-          <div className="">Packages</div>
-          <div className="grid grid-cols-3 gap-5 mt-5">
-            {event?.packages?.map((eventPackage) => {
-              return (
-                <div className="flex flex-col p-2 ring-1 ring-amber-500 rounded-md justify-center items-center space-y-3" onClick={()=>{
-                  alert(eventPackage?.title)
-                }}>
-                  <div className="text-sm font-semibold capitalize">
-                    {eventPackage?.title.toUpperCase()}
-                  </div>
-                  <div className="text-base font-semibold">
-                    {eventPackage?.price?.toLocaleString()}{" "}
-                    {eventPackage?.currency}
-                  </div>
-                  <div className="p-2 rounded-md bg-amber-400 w-full justify-center items-center flex m-5 cursor-pointer shadow-sm">
-                    Buy
-                  </div>
+            <div className="rounded-md bg-white flex flex-col py-5 px-8 mt-5">
+              <div className="">Packages</div>
+              <div className="grid grid-cols-3 gap-5 mt-5">
+                {event?.packages?.map((eventPackage) => {
+                  return (
+                    <div
+                      className="flex flex-col p-2 ring-1 ring-amber-500 rounded-md justify-center items-center space-y-3"
+                      onClick={() => {
+                        alert(eventPackage?.title);
+                      }}
+                    >
+                      <div className="text-sm font-semibold capitalize">
+                        {eventPackage?.title.toUpperCase()}
+                      </div>
+                      <div className="text-base font-semibold">
+                        {eventPackage?.price?.toLocaleString()}{" "}
+                        {eventPackage?.currency}
+                      </div>
+                      <div className="p-2 rounded-md bg-amber-400 w-full justify-center items-center flex m-5 cursor-pointer shadow-sm">
+                        Buy
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="rounded space-y-[2px]">
+            <div className="py-5 px-3 bg-white text-sm font-semibold">
+              Event details
+            </div>
+            <div className="py-5 px-3 bg-white flex flex-col space-y-5">
+              <div className="flex flex-row items-center space-x-5">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="0.8"
+                    stroke="currentColor"
+                    className="w-10 h-10 text-amber-700"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                    />
+                  </svg>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
+                <div className="flex flex-col text-sm font-thin">
+                  <div className="font-light">Date and time</div>
+                  <div>{event?.date}</div>
+                </div>
+              </div>
 
-      <div className="rounded space-y-[2px]">
-        <div className="py-5 px-3 bg-white text-sm font-semibold">
-          Event details
-        </div>
-        <div className="py-5 px-3 bg-white flex flex-col space-y-5">
-          <div className="flex flex-row items-center space-x-5">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="0.8"
-                stroke="currentColor"
-                className="w-10 h-10 text-amber-700"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                />
-              </svg>
-            </div>
-            <div className="flex flex-col text-sm font-thin">
-              <div className="font-light">Date and time</div>
-              <div>{event?.date}</div>
-            </div>
-          </div>
+              {breakLine}
 
-          {breakLine}
+              <div className="flex flex-row items-center space-x-5">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="0.8"
+                    stroke="currentColor"
+                    className="w-10 h-10 text-amber-700"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </div>
+                <div className="flex flex-col text-sm font-thin">
+                  <div className="font-light">Status</div>
+                  <div>{event?.status}</div>
+                </div>
+              </div>
 
-          <div className="flex flex-row items-center space-x-5">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="0.8"
-                stroke="currentColor"
-                className="w-10 h-10 text-amber-700"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </div>
-            <div className="flex flex-col text-sm font-thin">
-              <div className="font-light">Status</div>
-              <div>{event?.status}</div>
-            </div>
-          </div>
+              {breakLine}
+              <div className="flex flex-row items-center space-x-5">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="0.8"
+                    stroke="currentColor"
+                    className="w-10 h-10 text-amber-700"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
+                    />
+                  </svg>
+                </div>
+                <div className="flex flex-col text-sm font-thin">
+                  <div className="font-light">Location</div>
+                  <div>{event?.location}</div>
+                </div>
+              </div>
 
-          {breakLine}
-          <div className="flex flex-row items-center space-x-5">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="0.8"
-                stroke="currentColor"
-                className="w-10 h-10 text-amber-700"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
-                />
-              </svg>
-            </div>
-            <div className="flex flex-col text-sm font-thin">
-              <div className="font-light">Location</div>
-              <div>{event?.location}</div>
-            </div>
-          </div>
-
-          {breakLine}
-          <div className="flex flex-row items-center space-x-5">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="0.8"
-                stroke="currentColor"
-                className="w-10 h-10 text-amber-700"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </div>
-            <div className="flex flex-col text-sm font-thin">
-              <div className="font-light">Artist</div>
-              <div>{event?.artist}</div>
+              {breakLine}
+              <div className="flex flex-row items-center space-x-5">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="0.8"
+                    stroke="currentColor"
+                    className="w-10 h-10 text-amber-700"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </div>
+                <div className="flex flex-col text-sm font-thin">
+                  <div className="font-light">Artist</div>
+                  <div>{event?.artist}</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+      {loadingEventDetails && (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div role="status">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+        </div>
+      )}
+    </>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import useNextBlurhash from "use-next-blurhash";
 
-export let url = process.env.NEXT_PUBLIC_TICKETS_BCKEND_URL
+export let url = process.env.NEXT_PUBLIC_TICKETS_BCKEND_URL;
 // export const events = [
 //   {
 //     _id: "1",
@@ -73,7 +74,7 @@ export async function getEvents() {
   if (res.ok) {
     return res.json();
   } else {
-    console.log(res.statusText)
+    console.log(res.statusText);
     // throw Error(res.statusText);
   }
 }
@@ -81,107 +82,141 @@ export async function getEvents() {
 export default function Home() {
   let routert = useRouter();
   let [events, setEvents] = useState([]);
+  const [blurDataUrl] = useNextBlurhash("LEHV6nWB2yk8pyo0adR*.7kCMdnj");
+  let [loadingEvents, setLoadingEvents] = useState(true);
+
   useEffect(() => {
+    setLoadingEvents(true);
     getEvents().then((res) => {
       setEvents(res);
+      setLoadingEvents(false);
     });
   }, []);
   return (
     <main className="flex h-screen flex-col p-24">
       <div className="text-2xl font-semibold mb-5">Upcoming Events</div>
       {/* Event Cards */}
-      <div className="grid grid-cols-3 gap-4">
-        {events?.map((event) => {
-          return (
-            <div
-              className="rounded-md shadow-sm cursor-pointer hover:shadow-lg"
-              onClick={() => {
-                routert.push(`/events/${event._id}`);
-              }}
-            >
-              <div className="flex flex-col">
-                {/* Picture */}
-                <Image src={event.imageUrl} height={300} width={500} />
-              </div>
-              <div className="bg-white flex flex-col p-5 rounded-b-md space-y-4">
-                {/* Title */}
-                <div className="text-md font-semibold">{event.title}</div>
+      {loadingEvents && (
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
+      {!loadingEvents && (
+        <div className="grid grid-cols-3 gap-4">
+          {events?.map((event) => {
+            return (
+              <div
+                className="rounded-md shadow-sm cursor-pointer hover:shadow-lg"
+                onClick={() => {
+                  routert.push(`/events/${event._id}`);
+                }}
+              >
+                <div className="flex flex-col">
+                  {/* Picture */}
+                  <Image
+                    src={event.imageUrl}
+                    height={300}
+                    width={500}
+                    blurDataURL={blurDataUrl}
+                    placeholder="blur"
+                  />
+                </div>
+                <div className="bg-white flex flex-col p-5 rounded-b-md space-y-4">
+                  {/* Title */}
+                  <div className="text-md font-semibold">{event.title}</div>
 
-                {/* Ente date and time */}
+                  {/* Ente date and time */}
 
-                <div className="grid grid-cols-2 gap-3">
-                  <div className="flex flex-row items-center space-x-1">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="w-4 h-4 text-amber-700"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                        />
-                      </svg>
+                  <div className="grid grid-cols-2 gap-3">
+                    <div className="flex flex-row items-center space-x-1">
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-4 h-4 text-amber-700"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="text-sm text-gray-700">{event.date}</div>
                     </div>
-                    <div className="text-sm text-gray-700">{event.date}</div>
-                  </div>
 
-                  <div className="flex flex-row items-center space-x-1">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="w-4 h-4 text-amber-700"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
+                    <div className="flex flex-row items-center space-x-1">
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-4 h-4 text-amber-700"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="text-sm text-gray-700">{event.time}</div>
                     </div>
-                    <div className="text-sm text-gray-700">{event.time}</div>
-                  </div>
 
-                  <div className="flex flex-row items-center space-x-1">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="w-4 h-4 text-amber-700"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                        />
-                      </svg>
-                    </div>
-                    <div className="text-sm text-gray-700">
-                      {event.location}
+                    <div className="flex flex-row items-center space-x-1">
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-4 h-4 text-amber-700"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                          />
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="text-sm text-gray-700">
+                        {event.location}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </main>
   );
 }
