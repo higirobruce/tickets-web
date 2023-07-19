650972ef6f880bed862380dd7ab1e8429d9bad10
Bruce <higirobru@gmail.com>
2023-07-19 11:41:51 +0200

    Handle consumed tickets

--- app/tickets/[id]/page.js
+++ app/tickets/[id]/page.js
@@ -9,6 +9,7 @@ import {
   TicketIcon,
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import dayjs from "dayjs";
 
 export async function getTicketDetails(id, setLoading) {
   setLoading(true);
@@ -234,6 +235,29 @@ export default function page() {
               </button>
 
               {consuming && buidLoader}
+            </div>
+          )}
+
+          {ticket?.status === "consumed" && !confirming && (
+            <div className="md:w-1/3 w-full flex flex-col items-center text-sm bg-green-500 rounded p-3 text-white ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-10 h-10 mb-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+                />
+              </svg>
+              <div className="mb-2">
+                Ticket already consumed at{" "}
+                {dayjs(ticket?.updatedAt).format("YYYY-MMM-DD HH:mm:ss a")}
+              </div>
             </div>
           )}
         </div>
